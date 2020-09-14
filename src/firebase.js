711cd0d2bc461c8eb9225d22a89fd4b893074615
iamEJ <jukneviciute185@gmail.com>
2020-09-14 19:41:45 +0300

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC4tZ7Lr-IfhgfgexCQbjzp9uodAoA9DCA",
+  authDomain: "facebook-clone-cb2c3.firebaseapp.com",
+  databaseURL: "https://facebook-clone-cb2c3.firebaseio.com",
+  projectId: "facebook-clone-cb2c3",
+  storageBucket: "facebook-clone-cb2c3.appspot.com",
+  messagingSenderId: "1064691472068",
+  appId: "1:1064691472068:web:5d8bdf2bbbb6a6d9f71b91",
+  measurementId: "G-SZGWF998LX",
+};
