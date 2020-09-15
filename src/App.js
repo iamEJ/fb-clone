0869acd935604ea19cf8d37054e0e7b80bceb8b9
iamEJ <jukneviciute185@gmail.com>
2020-09-15 18:27:24 +0300

    adding page header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Facebook clone</h1>
+      {/* Header */}
+      <Header />
+      {/* App body */}
+      {/* Sidebar */}
+      {/* Feeds */}
+      {/* Widges */}
     </div>
   );
 }
