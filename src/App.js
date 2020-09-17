75957391d2cfcfb83f27a1179b220392557c2e49
iamEJ <jukneviciute185@gmail.com>
2020-09-17 20:19:38 +0300

    add feed component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
 function App() {
   return (
     <div className="app">
       <Header />
       <div>
         <Sidebar />
+        <Feed />
       </div>
-      {/* App body */}
-      {/* Sidebar */}
-      {/* Feeds */}
       {/* Widges */}
     </div>
   );
---
+++ src/components/Feed.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/Feed.css";
+
+function Feed() {
+  return (
+    <div className="feed">
+      <h1>Feed</h1>
+    </div>
+  );
+}
+
+export default Feed;
